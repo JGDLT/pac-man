49c8e3da0815208080788d5852cb7ea7ef6d7b55
JGDLT <91678501+JGDLT@users.noreply.github.com>
2022-02-24 07:55:53 -0700

    Update pacmancode.cpp

--- pacmancode.cpp
+++ pacmancode.cpp
@@ -21,25 +21,28 @@ int main() {
     int map[12][12] = {
         1,1,1,1,1,1,1,1,1,1,1,1,
         1,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,0,1,0,1,0,1,1,0,1,
+        1,0,0,0,1,0,1,0,0,1,0,1,
+        1,0,1,1,1,0,1,0,1,1,0,1,
+        1,0,1,0,0,0,1,0,1,0,0,1,
+        1,0,1,0,0,1,1,0,1,0,0,1,
+        1,0,1,1,0,1,0,0,0,0,0,1,
+        1,0,0,0,0,0,0,0,1,0,0,1,
+        1,0,1,1,0,1,1,0,0,1,0,1,
         1,0,0,0,0,0,0,0,0,0,0,1,
         1,1,1,1,1,1,1,1,1,1,1,1
     };
 
     //player setup
-    int xpos = 200;
-    int ypos = 200;
+    int xpos = 95;
+    int ypos = 95;
     int vx = 0;
     int vy = 0;
-    sf::CircleShape player(35);
+    int radius = 35;
+    sf::CircleShape dot(8);
+    sf::CircleShape player(radius);
     player.setFillColor(sf::Color(250, 250, 0)); //using RGB value for color here (hex also works)
+    dot.setFillColor(sf::Color(0, 0, 250));
     player.setPosition(xpos, ypos); //top left "corner" of circle (not center!)
     bool keys[] = { false, false, false, false };
 
@@ -52,7 +55,7 @@ int main() {
             //this checks if the user has clicked the little "x" button in the top right corner
             if (event.type == sf::Event::EventType::Closed)
                 screen.close();
-            //KEYBOARD INPUT 
+            //KEYBOARD INPUT
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
                 keys[LEFT] = true;
             }
@@ -76,7 +79,7 @@ int main() {
 
         }//end event loop---------------------------------------------------------------
 
-         //move Mr. Pac
+         //movePac
         if (keys[LEFT] == true)
             vx = -5;
 
@@ -91,9 +94,59 @@ int main() {
             vy = 5;
 
         else vy = 0;
+
+
+        //RIGHT COLLISION
+        if (vx > 0 &&
+            //check if TOP right corner has hit
+            ((map[(ypos) / 80]
+                [(xpos + (radius * 2) + 5) / 80] == 1) ||
+                (map[(ypos + (radius * 2) - 5) / 80]
+                    [(xpos + (radius * 2) + 5) / 80] == 1))) {
+            vx = 0;
+            cout << "colliding RIGHT";
+        }
+         
+        //LEFT collision
+        else if (vx < 0 &&
+             ((map[(ypos + 5) / 80]
+                [(xpos - 5) / 80] == 1) ||
+                (map[(ypos + (radius * 2)-5) / 80]
+                    [(xpos - 5) / 80] == 1))) {
+            vx = 0;
+            cout << "colliding LEFT ";
+        }
+        //DOWN collision
+        if (vy > 0 &&
+               ((map[(ypos + (radius * 2) + 5) / 80]
+                    [(xpos) / 80] == 1) ||
+                (map[(ypos + (radius * 2) + 5) / 80]
+                    [(xpos + (radius * 2)) / 80] == 1))) {
+            vy = 0;
+            cout << "colliding DOWN";
+        }
+        //UP collision
+        else if (vy < 0 &&
+               ((map[(ypos - 5) / 80]
+                    [(xpos+5 ) / 80] == 1) ||
+                (map[(ypos - 5) / 80]
+                    [(xpos + (radius * 2)) / 80] == 1))) {
+            vy = 0;
+            cout << "colliding UP";
+        }
+
+        //player update position
         xpos += vx;
         ypos += vy;
         player.setPosition(xpos, ypos);
+
+
+        //dots
+        if (map[ypos / 80][xpos / 80] == 0)
+            map[ypos / 80][xpos / 80] = -1;
+        cout << xpos << ", " << ypos << endl;
+
+
 
 
         //render section-----------------------------------------
@@ -105,6 +158,10 @@ int main() {
                 if (map[rows][cols] == 1) {
                     wall.setPosition(cols * 80, rows * 80);
                     screen.draw(wall);
+                }
+                if (map[rows][cols] == 0) {
+                    dot.setPosition(cols * 80 + 40, rows * 80 + 40);
+                    screen.draw(dot);
                 }
             }
         screen.draw(player); //draw player
